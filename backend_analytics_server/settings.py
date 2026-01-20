3d9ae1ec624530b14c2ee08708ddf0378e5534cd
Enrique2305-gif <egrosado@espol.edu.ec>
2026-01-20 03:03:25 +0000

    prueba guia27

--- backend_analytics_server/settings.py
+++ backend_analytics_server/settings.py
@@ -74,6 +74,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "backend_analytics_server.wsgi.application"
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
