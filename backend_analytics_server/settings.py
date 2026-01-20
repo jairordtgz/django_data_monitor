62455445c98df0f0eab4ef34c99ee26ae11afb52
Enrique2305-gif <egrosado@espol.edu.ec>
2026-01-20 02:30:29 +0000

    guia27 avance

--- backend_analytics_server/settings.py
+++ backend_analytics_server/settings.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+import pymysql
+pymysql.install_as_MySQLdb()
 
 from pathlib import Path
 import os 
-import pymysql
 
-pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
