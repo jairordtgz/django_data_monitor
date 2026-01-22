97cb646fdf16726d1d075ba5443d9d4f86ae5f7f
Enrique2305-gif <egrosado@espol.edu.ec>
2026-01-22 03:29:05 +0000

    intento

--- backend_analytics_server/settings.py
+++ backend_analytics_server/settings.py
@@ -15,6 +15,8 @@ pymysql.install_as_MySQLdb()
 from pathlib import Path
 import os 
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -80,11 +82,11 @@ WSGI_APPLICATION = "backend_analytics_server.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 
