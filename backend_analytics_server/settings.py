fac85a9ca555a60db4dad0392b19eb05cd1909fd
Enrique2305-gif <egrosado@espol.edu.ec>
2026-01-22 06:04:58 +0000

    intento28

--- backend_analytics_server/settings.py
+++ backend_analytics_server/settings.py
@@ -80,12 +80,12 @@ WSGI_APPLICATION = "backend_analytics_server.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default="sqlite:///db.sqlite3",
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3"),
         conn_max_age=600,
-        ssl_require=True
     )
 }
+
 
 
 # Password validation
