9611e0b878e284b2d1b91c042a862ac84ae90493
Enrique2305-gif <egrosado@espol.edu.ec>
2026-01-22 06:15:12 +0000

    intento29

--- backend_analytics_server/settings.py
+++ backend_analytics_server/settings.py
@@ -80,10 +80,14 @@ WSGI_APPLICATION = "backend_analytics_server.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.parse(
-        os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3"),
-        conn_max_age=600,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQLDATABASE'),
+        'USER': os.environ.get('MYSQLUSER'),
+        'PASSWORD': os.environ.get('MYSQLPASSWORD'),
+        'HOST': os.environ.get('MYSQLHOST'),
+        'PORT': os.environ.get('MYSQLPORT'),
+     }
 }
 
 
